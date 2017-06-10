9e474a3eb1f55771aca66871d375483d561cb48b
Travis Bay <tb007magetech@gmail.com>
2017-06-09 23:29:44 -0400

    test

--- LightningTalkNative/LightningTalkNative.Android/MainActivity.cs
+++ LightningTalkNative/LightningTalkNative.Android/MainActivity.cs
@@ -19,7 +19,7 @@ namespace LightningTalkNative.Droid
 			base.OnCreate (bundle);
 
 			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			SetContentView (Resource.Layout.Main); 
 
 			// Get our button from the layout resource,
 			// and attach an event to it
